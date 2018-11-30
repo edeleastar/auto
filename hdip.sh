86c6c0af1e9fc2570f3a108fc000a35726556209
Eamonn de Leastar <edeleastar@wit.ie>
2018-11-30 10:19:08 +0000

    adjust path variables

--- hdip.sh
+++ hdip.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 
-srcPostfix="-2018"
-localRoot="/Users/edeleastar/repos/modules/wit-hdip-comp-sci/2018"
+year="2019"
+
+srcPostfix="-${year}"
+localRoot="/Users/edeleastar/repos/modules/wit-hdip-comp-sci/${year}"
 remoteRouteSrc="https://edel020@bitbucket.org/wit-hdip-comp-sci"
 remoteRoutePublic="https://github.com/wit-hdip-comp-sci${srcPostfix}"
 
-modules="computer-systems
-database
-ict-skills
-programming
-web-development
-june-onsite"
+#modules="computer-systems
+#database
+#ict-skills
+#programming
+#web-development
+#june-onsite"
 
+
+modules="programming
+web-development"
 
 . /Users/edeleastar/bin/auto/lib.sh
 
 #cloneAll
 #pullAll
-#generateAll
-publishAll
+generateAll
+#publishAll
